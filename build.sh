0e93e77ab218b2364dabd755169c8d3aed819cb6
Chris Richardson <chris@chrisrichardson.net>
2021-01-29 11:44:08 -0800

    Published documentation

---
+++ build.sh
@@ -0,0 +1,7 @@
+#! /bin/bash -e
+
+if [ ! -d "public" ] ; then
+  git worktree add -B gh-pages public origin/gh-pages
+fi
+
+asciidoctor docs/microservices-assessment-platform-api.adoc -b html -o public/index.html -a allow-uri-read
