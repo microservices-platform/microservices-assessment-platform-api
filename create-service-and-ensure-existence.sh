0f86be97f1ebacce98cb40222d2f78725e5038de
mxzpytlk <mxzpytlk@gmail.com>
2022-01-11 14:12:07 +0300

    Issue microservices-platform/microservices-assesment-platform#1207 GraphQL: create a demo shell script that illustrates integration with CircleCI (for example): script -Added curl which set github repository metadata to service

--- create-service-and-ensure-existence.sh
+++ create-service-and-ensure-existence.sh
@@ -1,9 +1,11 @@
-organization=$1
-application=$2
-service=$3
+#! /bin/bash -e
 
-curl -X POST "$API_ENDPOINT" \
--u $USER_LOGIN:$USER_PASSWORD \
+application=${1?}
+service=${2?}
+organization=$3
+
+curl -X POST "$MSAT_API_ENDPOINT" \
+-u $MSAT_USER_LOGIN:$MSAT_USER_PASSWORD \
 -d '
   {
     "query": "mutation findOrCreateServiceByName ($serviceName: ServiceName!) { findOrCreateServiceByName (serviceName: $serviceName) { organizationId applicationId serviceId } }",
@@ -12,6 +14,25 @@ curl -X POST "$API_ENDPOINT" \
           "organization": "'"$organization"'",
           "application": "'"$application"'",
           "service": "'"$service"'"
+      }
+    }
+  }
+'
+
+github_repo=`git remote -v | grep -o "origin\s.*\s(fetch)" | awk '{ print $2 }'`
+
+curl -X POST "$MSAT_API_ENDPOINT" \
+-u $MSAT_USER_LOGIN:$MSAT_USER_PASSWORD \
+-d '
+  {
+    "query": "mutation updateServiceMetadata ($organization: String, $applicationName: String!, $serviceName: String!, $metadata: [MetadataInput!]!) {\n    updateServiceMetadata (organization: $organization, applicationName: $applicationName, serviceName: $serviceName, metadata: $metadata)\n}",
+    "variables": {
+      "organization": "'"$organization"'",
+      "applicationName": "'"$application"'",
+      "serviceName":"'"$service"'",
+      "metadata": {
+        "name": "github-repo",
+        "value": "'"$github_repo"'"
       }
     }
   }
